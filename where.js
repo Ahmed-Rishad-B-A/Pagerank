79e53829f07068b7ed500897414361eeb02f5b6f
Ahmed Rishad B A <56973548+Ahmed-Rishad-B-A@users.noreply.github.com>
2020-05-01 17:12:59 +0530

    geodata uploaded

---
+++ where.js
@@ -0,0 +1,9 @@
+myData = [
+[12.5020515,75.0873956, 'Bovikanam, Kerala 671542, India'],
+[9.9312328,76.26730409999999, 'Kochi, Kerala, India'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[33.9094132,-83.4603953, '2500 Daniells Bridge Rd #300, Athens, GA 30606, USA']
+];
